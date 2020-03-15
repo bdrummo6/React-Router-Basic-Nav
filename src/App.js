30c3a8e7be6f469793b91d62288c89e5a2de1640
bdrummo6 <bradley.drummonds@gmail.com>
2020-03-15 17:55:07 -0400

    Added some styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Route } from 'react-router-dom'; // Import so I can create my Routes
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
-  <div>
-    <Navigation />
-    <Route exact path='/' component={Home} /> {/* Set the Route for the Home component */}
-    <Route path='/about' component={About} /> {/* Set the Route for the About component */}
-    <Route path='/contact' component={Contact} /> {/* Set the Route for the Contact component */}
-  </div>
-);
+const App = () => {
+
+   return (
+      <div className='App'>
+         <Navigation/>
+         <Route exact path='/' component={Home}/> {/* Set the Route for the Home component */}
+         <Route path='/about' component={About}/> {/* Set the Route for the About component */}
+         <Route path='/contact' component={Contact}/> {/* Set the Route for the Contact component */}
+      </div>
+   )
+};
 
 export default App;
